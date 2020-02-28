3f197540580f42aefa4ccaaa15a017251368a74a
jansenv <jansenv44@gmail.com>
2020-02-28 14:44:50 -0600

    setup for idioms

---
+++ exercises/idioms/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace idioms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> idioms = new Dictionary<string, List<string>>();
+        }
+    }
+}
