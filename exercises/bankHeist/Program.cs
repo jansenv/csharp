a5456fa858b1dbc7f38a3cd42306acdd7e99d309
jansenv <jansenv44@gmail.com>
2020-03-02 10:31:40 -0600

    formatting

--- exercises/bankHeist/Program.cs
+++ exercises/bankHeist/Program.cs
@@ -8,21 +8,30 @@ namespace bankHeist
         static void Main(string[] args)
         {
             Console.WriteLine("Plan Your Heist!");
-            Dictionary<string, string> teamMember = new Dictionary<string, string>();
+            List<Dictionary<string, string>> teamMembers = new List<Dictionary<string, string>>();
 
-            Console.WriteLine("Add your heist member: ");
-            string singleMemberName = Console.ReadLine();
-            teamMember.Add("name", singleMemberName);
+            while (true)
+            {
+                Dictionary<string, string> teamMember = new Dictionary<string, string>();
 
-            Console.WriteLine("Enter the member's skill level (0-100): ");
-            string singleMemberSkill = Console.ReadLine();
-            teamMember.Add("skill level", singleMemberSkill);
+                Console.WriteLine("Add your heist member: ");
+                string memberName = Console.ReadLine();
+                if (memberName == "")
+                {
+                    break;
+                }
+                teamMember.Add("name", memberName);
 
-            Console.WriteLine("Enter the member's courage factor (0.0-2.0): ");
-            string singleMemberCourage = Console.ReadLine();
-            teamMember.Add("courage factor", singleMemberCourage);
+                Console.WriteLine("Enter the member's skill level (0-100): ");
+                string memberSkill = Console.ReadLine();
+                teamMember.Add("skill level", memberSkill);
 
-            Console.WriteLine($"Member: {singleMemberName}; Member Skill: {singleMemberSkill}; Member Courage: {singleMemberCourage}");
+                Console.WriteLine("Enter the member's courage factor (0.0-2.0): ");
+                string memberCourage = Console.ReadLine();
+                teamMember.Add("courage factor", memberCourage);
+
+                Console.WriteLine($"Member: {memberName}; Member Skill: {memberSkill}; Member Courage: {memberCourage}");
+            }
         }
     }
 }
