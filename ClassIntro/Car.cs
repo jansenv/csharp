4c369bfcb651d3a061257e07ddb0eff7bcda6abd
jansenv <jansenv44@gmail.com>
2020-03-05 10:53:38 -0600

    class intros

---
+++ ClassIntro/Car.cs
@@ -0,0 +1,68 @@
+using System;
+
+namespace ClassIntro
+{
+    public class Car
+    {
+        private int _milesDriven = 0;
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public bool NeedsMaintenance { get; set; } = false;
+        public bool IsClean { get; set; } = false;
+
+        // Computed Property
+        // It's a readonly property. Cannot be set.
+        public string Description
+        {
+            get
+            {
+                return $"{Year} {Make} {Model}";
+            }
+        }
+
+        // Methods
+        public void Drive(int miles)
+        {
+            _milesDriven += miles;
+
+            if (_milesDriven > 10000)
+            {
+                NeedsMaintenance = true;
+            }
+        }
+
+        public void Service()
+        {
+            NeedsMaintenance = false;
+        }
+
+        // Method overloading.
+        // Shares a method name, but has different arguments
+        public void Service(bool addCleaningService)
+        {
+            NeedsMaintenance = false;
+
+            if (addCleaningService)
+            {
+                IsClean = true;
+            }
+        }
+
+
+        // This is a constructor. It doesn't have a return type
+        // and the name of the method is the same as the name of the class
+        public Car(int milesDriven)
+        {
+            _milesDriven = milesDriven;
+        }
+
+        public Car(int milesDriven, string make, string model, int year)
+        {
+            _milesDriven = milesDriven;
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+    }
+}
---
+++ ClassIntro/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myFordBronco = new Car(40000, "Ford", "Bronco", 1982);
+
+            // You "set" the Make here
+            Car mySubaru = new Car(10000)
+            {
+                Make = "Subaru",
+                Model = "Outback",
+                Year = 1999
+            };
+
+            // You "get" the Make here
+            Console.WriteLine(mySubaru.Make);
+
+            Console.WriteLine(mySubaru.Description);
+            Console.WriteLine(myFordBronco.Description);
+
+            // Invoke the Drive method
+            myFordBronco.Drive(9999);
+
+            Console.WriteLine($"Bronco needs service: {myFordBronco.NeedsMaintenance}");
+
+            myFordBronco.Drive(60);
+
+            Console.WriteLine($"Bronco needs service: {myFordBronco.NeedsMaintenance}");
+
+            myFordBronco.Service(true);
+
+        }
+    }
+}
