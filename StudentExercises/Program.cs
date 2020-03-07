8aac0ab74df08066006f21476f7461c3e41da4a7
jansenv <jansenv44@gmail.com>
2020-03-06 18:18:51 -0600

    added exercises

--- StudentExercises/Program.cs
+++ StudentExercises/Program.cs
@@ -6,10 +6,29 @@ namespace StudentExercises
     {
         static void Main(string[] args)
         {
-            new Exercise()
+            Exercise MartinsAquarium = new Exercise()
             {
+                name = "MartinsAquarium",
+                language = "JavaScript"
+            };
 
-            }
+            Exercise CongressionalRep = new Exercise()
+            {
+                name = "Congressional Representative",
+                language = "HTML"
+            };
+
+            Exercise Nutshell = new Exercise()
+            {
+                name = "Nutshell",
+                language = "React"
+            };
+
+            Exercise Buildings = new Exercise()
+            {
+                name = "Buildings",
+                language = "C#"
+            };
         }
     }
 }
