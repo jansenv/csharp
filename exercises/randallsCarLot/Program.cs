b15e620224cefb1081ad0f88f04d9b915c48e852
jansenv <jansenv44@gmail.com>
2020-03-08 12:02:06 -0500

    refactoring

--- exercises/randallsCarLot/Program.cs
+++ exercises/randallsCarLot/Program.cs
@@ -7,55 +7,60 @@ namespace randallsCarLot
     {
         static void Main(string[] args)
         {
-            List<Dictionary<string, Dictionary<string, string>>> vehicles = new List<Dictionary<string, Dictionary<string, string>>>();
+            List<Dictionary<string, Dictionary<string, string>>> ListOfVehicles = new List<Dictionary<string, Dictionary<string, string>>>();
 
-            Dictionary<string, Dictionary<string, string>> sale = new Dictionary<string, Dictionary<string, string>>();
-            Dictionary<string, string> vehicle = new Dictionary<string, string>();
-            Dictionary<string, string> salesAgent = new Dictionary<string, string>();
-            Dictionary<string, string> credit = new Dictionary<string, string>();
+            var vehicleObject1 = new Dictionary<string, Dictionary<string, string>>();
 
-            vehicle.Add("year", "2008");
-            vehicle.Add("model", "Damfresh");
-            vehicle.Add("make", "Biotraxquote");
-            vehicle.Add("color", "sky magenta");
-            salesAgent.Add("mobile", "(896) 478-6975");
-            salesAgent.Add("Last Name", "Botsford");
-            salesAgent.Add("First Name", "Shaina");
-            credit.Add("Credit Provider", "J.P.Morgan Chase & Co");
-            credit.Add("Account", "601109582720302");
+            var vehicle1 = new Dictionary<string, string>();
+            var salesAgent1 = new Dictionary<string, string>();
+            var credit1 = new Dictionary<string, string>();
 
-            sale.Add("vehicle", vehicle);
-            sale.Add("sales agent", salesAgent);
-            sale.Add("credit", credit);
+            vehicle1.Add("year", "2008");
+            vehicle1.Add("model", "Damfresh");
+            vehicle1.Add("make", "Biotraxquote");
+            vehicle1.Add("color", "sky magenta");
 
-            vehicles.Add(sale);
+            salesAgent1.Add("mobile", "(896) 478-6975");
+            salesAgent1.Add("LastName", "Botsford");
+            salesAgent1.Add("FirstName", "Shaina");
 
-            Dictionary<string, Dictionary<string, string>> sale2 = new Dictionary<string, Dictionary<string, string>>();
+            credit1.Add("CreditProvider", "J.P.Morgan Chase & Co");
+            credit1.Add("Account", "601109582720302");
 
-            Dictionary<string, string> vehicle2 = new Dictionary<string, string>();
-            Dictionary<string, string> salesAgent2 = new Dictionary<string, string>();
-            Dictionary<string, string> credit2 = new Dictionary<string, string>();
+            vehicleObject1.Add("vehicle", vehicle1);
+            vehicleObject1.Add("sales agent", salesAgent1);
+            vehicleObject1.Add("credit", credit1);
+
+            ListOfVehicles.Add(vehicleObject1);
+
+            var vehicleObject2 = new Dictionary<string, Dictionary<string, string>>();
+
+            var vehicle2 = new Dictionary<string, string>();
+            var salesAgent2 = new Dictionary<string, string>();
+            var credit2 = new Dictionary<string, string>();
 
             vehicle2.Add("year", "2010");
             vehicle2.Add("model", "Hotquadtrax");
             vehicle2.Add("make", "Transtintechno");
             vehicle2.Add("color", "robin egg blue");
+
             salesAgent2.Add("mobile", "562.300.2912");
             salesAgent2.Add("lastName", "Davis");
             salesAgent2.Add("firstName", "Gerardo");
+
             credit2.Add("creditProvider", "PNC Financial Services");
             credit2.Add("account", "34578280562836");
 
-            vehicles.Add(sale2);
+            ListOfVehicles.Add(vehicleObject2);
 
-            foreach (Dictionary<string, Dictionary<string, string>> saleDic in vehicles)
+            foreach (Dictionary<string, Dictionary<string, string>> vehicleObject in ListOfVehicles)
             {
-                foreach (KeyValuePair<string, Dictionary<string, string>> salePair in saleDic)
+                foreach (KeyValuePair<string, Dictionary<string, string>> objectKVpair in vehicleObject)
                 {
-                    Console.WriteLine(salePair.Key);
-                    foreach (KeyValuePair<string, string> section in salePair.Value)
+                    Console.WriteLine(objectKVpair.Key);
+                    foreach (KeyValuePair<string, string> objectDetail in objectKVpair.Value)
                     {
-                        Console.WriteLine($"{section.Key}: {section.Value}");
+                        Console.WriteLine($"{objectDetail.Key}: {objectDetail.Value}");
                     }
                 }
             }
