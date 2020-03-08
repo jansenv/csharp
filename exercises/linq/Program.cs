016d355a7aeafd7fb493a5457f6db6d35e740255
jansenv <jansenv44@gmail.com>
2020-03-08 18:05:33 -0500

    finished operation 2

--- exercises/linq/Program.cs
+++ exercises/linq/Program.cs
@@ -16,11 +16,24 @@ namespace linq
             where fruit.StartsWith("L")
             select fruit;
 
-            foreach (string fruit in Lfruits)
-            {
-                Console.WriteLine($"{fruit}");
-            }
+            // foreach (string fruit in Lfruits)
+            // {
+            //     Console.WriteLine($"{fruit}");
+            // }
 
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            IEnumerable<int> fourSixMultiples =
+            numbers.Where(n => n % 4 == 0 || n % 6 == 0).OrderBy(n => n);
+
+            foreach (int multiple in fourSixMultiples)
+            {
+                Console.WriteLine($"{multiple}");
+            }
 
         }
     }
