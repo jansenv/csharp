89a0f4215ef53016d110889111caec7d70d0da21
jansenv <jansenv44@gmail.com>
2020-03-08 21:09:54 -0500

    finished bank thing

--- exercises/linq/Program.cs
+++ exercises/linq/Program.cs
@@ -4,9 +4,22 @@ using System.Linq;
 
 namespace linq
 {
-    class Program
+
+    public class Customer
     {
-        static void Main(string[] args)
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string Bank { get; set; }
+    }
+
+    public class Bank
+    {
+        public string BankName { get; set; }
+        public int HowManyMillionaires { get; set; }
+    }
+    public class Program
+    {
+        public static void Main()
         {
             // RESTRICTION / FILTERING OPERATIONS
 
@@ -114,17 +127,17 @@ namespace linq
 
             // PARTITIONING OPERATIONS
 
-            List<int> wheresSquaredo = new List<int>()
-            {
-                66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
-            };
+            // List<int> wheresSquaredo = new List<int>()
+            // {
+            //     66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+            // };
 
-            IEnumerable<int> squares = wheresSquaredo.TakeWhile(num => num % Math.Sqrt(num) != 0);
+            // IEnumerable<int> squares = wheresSquaredo.TakeWhile(num => num % Math.Sqrt(num) != 0);
 
-            foreach (int sqrt in squares)
-            {
-                Console.WriteLine(sqrt);
-            }
+            // foreach (int sqrt in squares)
+            // {
+            //     Console.WriteLine(sqrt);
+            // }
 
             /*
             Store each number in the following List until a perfect square
@@ -133,7 +146,51 @@ namespace linq
             Expected output is { 66, 12, 8, 27, 82, 34, 7, 50, 19, 46 } 
 
             Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+            
             */
+
+            List<Customer> customers = new List<Customer>()
+            {
+            new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            };
+
+            /*
+            Given the same customer set, display how many millionaires per bank.
+            Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
+
+            Example Output:
+            WF 2
+            BOA 1
+            FTB 1
+            CITI 1
+            */
+
+            IEnumerable<Customer> millionaires =
+            from customer in customers
+            where customer.Balance >= 1000000.00
+            select customer;
+
+            IEnumerable<Bank> banksCount = millionaires
+            .GroupBy(customer => customer.Bank)
+            .Select(bank => new Bank
+            {
+                BankName = bank.Key,
+                HowManyMillionaires = bank.Count()
+            });
+
+            foreach (Bank bank in banksCount)
+            {
+                Console.WriteLine($"{bank.BankName} {bank.HowManyMillionaires}");
+            }
 
         }
     }
