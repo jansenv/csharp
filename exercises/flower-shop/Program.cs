4491f1b163d52e090f8382f4ecad2273cf5b03e4
jansenv <jansenv44@gmail.com>
2020-03-10 09:26:03 -0500

    built rose class

---
+++ exercises/flower-shop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace flower_shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ exercises/flower-shop/Rose.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace flower_shop
+{
+    class Rose
+    {
+        public string color { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public decimal cost { get; set; }
+        public string waterSchedule { get; set; }
+        public bool thorny { get; set; }
+        public bool fragrant { get; set; }
+    }
+}
