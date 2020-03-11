4635a4c183f5f94d422fb58d66b1e0b2852d6565
jansenv <jansenv44@gmail.com>
2020-03-11 13:23:22 -0500

    listed operatives

--- exercises/bankHeist2/Program.cs
+++ exercises/bankHeist2/Program.cs
@@ -7,13 +7,27 @@ namespace bankHeist2
     {
         static void Main(string[] args)
         {
+            // Create rolodex of robbers
             List<IRobber> rolodex = new List<IRobber>();
+
+            //Instantiate some robbers
             Hacker Audrey = new Hacker();
             Hacker Willy = new Hacker();
             LockSpecialist Holden = new LockSpecialist();
             LockSpecialist Kevin = new LockSpecialist();
             Muscle Aja = new Muscle();
             Muscle Garrett = new Muscle();
+
+            //Add robbers to the rolodex
+            rolodex.Add(Audrey);
+            rolodex.Add(Willy);
+            rolodex.Add(Holden);
+            rolodex.Add(Kevin);
+            rolodex.Add(Aja);
+            rolodex.Add(Garrett);
+
+            Console.WriteLine($"There are {rolodex.Count} current operatives.");
+            Console.ReadLine($"Who would you like to add to the rolodex?");
         }
     }
 }
