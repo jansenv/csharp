a9a7aba904ff5b92bfa9fe3e63fcc28be8523226
jansenv <jansenv44@gmail.com>
2020-03-11 10:03:26 -0500

    added BettaFish

---
+++ exercises/ZoologicalZaniness/animals/BettaFish.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace ZoologicalZaniness
+{
+    public class BettaFish : ISwimming, IMoving
+    {
+        public int MaximumDepth { get; } = 1;
+        public int MaximumSpeed { get; } = 2;
+        public int MaximumHeight { get; } = 5000;
+
+        public void Run()
+        {
+            Console.WriteLine("Animal is now running");
+        }
+        public void Walk()
+        {
+            Console.WriteLine("Animal is now walking");
+        }
+        public void Swim()
+        {
+            Console.WriteLine("Animal is now swimming");
+        }
+        public void Move()
+        {
+            Console.WriteLine("That boi is movin'");
+        }
+        public void Dig()
+        {
+            Console.WriteLine("DIG IT@!!!");
+        }
+        public void Fly()
+        {
+            Console.WriteLine("R. Kelly");
+        }
+    }
+}
