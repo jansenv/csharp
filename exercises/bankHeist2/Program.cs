056055b01c44e8787dda940967937aca5266a78a
jansenv <jansenv44@gmail.com>
2020-03-11 10:35:26 -0500

    initialized repo

---
+++ exercises/bankHeist2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bankHeist2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
